7f1f09e9e3ab790c03f418fd00deb66c229e96e2
iam-suryateja-singasani <suryatejacertificate@gmail.com>
2026-02-20 16:10:33 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -8,7 +8,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "hackathon-user7-terraform-state-20022026"
+    bucket  = "healthcare-terraform-state-user7"
     key     = "dev/terraform.tfstate"
     region  = "ap-south-1"
     encrypt = true
